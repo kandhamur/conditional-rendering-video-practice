c785160d488e417da084395631bedbdea72cb1a4
gireesh <gireeshnaidu2921996@gmail.com>
2022-12-25 17:14:05 +0000

    conditional rendering video practice

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import {Component} from 'react'
+import './App.css'
+
+class App extends Component {
+  state = {isLoggedIn: true}
+
+  renderAuthButton = () => {
+    const {isLoggedIn} = this.state
+    if (isLoggedIn === true) {
+      return <button>Logout</button>
+    }
+    return <button>Login</button>
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <Welcome greeting="Hello" name="User" />
+      </div>
+    )
+  }
+}
+
+export default App
